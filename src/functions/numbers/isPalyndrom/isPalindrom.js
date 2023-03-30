d3043c9946259d6d97492f14fef84a0ba11e7a1c
Hadrien <smethadrien.96@gmail.com>
2023-03-30 14:11:42 +0200

    Made a function to check if a number is a palindrom with a time complexity of O(n)

---
+++ src/functions/numbers/isPalyndrom/isPalindrom.js
@@ -0,0 +1,21 @@
+//How to make a function wich accepts a number as argument and wich returns true if it is a palindrom and false if it is not.
+export const isPalindrome = (x) => {
+    //TIME-COMPLEXITY: O(n);
+    //MEMORY: O(1);
+    if (typeof x !== "number") {
+        return null;
+    }
+
+    const str = x.toString();
+    let leftIndex = 0;
+    let rightIndex = str.length - 1;
+    while (leftIndex < rightIndex) {
+        if (str.charAt(leftIndex) !== str.charAt(rightIndex)) {
+            return false;
+        }
+        leftIndex++;
+        rightIndex--;
+    }
+
+    return true;
+};
---
+++ src/functions/numbers/isPalyndrom/isPalindrom.test.js
@@ -0,0 +1,23 @@
+import { isPalindrome } from "./isPalindrom";
+
+describe("numbers/isPalindrom", () => {
+    describe("with right argument", () => {
+        test("when it returns true", () => {
+            const example = 383;
+
+            expect(isPalindrome(example)).toBe(true);
+        });
+        test("when it returns false", () => {
+            const example = 583;
+
+            expect(isPalindrome(example)).toBe(false);
+        });
+    });
+    describe("with wrong argument", () => {
+        test("it returns null", () => {
+            const example = "595";
+
+            expect(isPalindrome(example)).toBe(null);
+        });
+    });
+});
